ed4de06a3ed71dbd735a4b0543777aaaf49df562
Yuliang Zhou <yuliangzhou7@gmail.com>
2016-09-08 15:04:35 +1200

    New branch

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -10,6 +10,7 @@ import javafx.stage.WindowEvent;
 public class Main extends Application {
 
     private static Stage _window;
+    //make non static?
 
     public static final String titleScreenID = "mainTitle";
     public static final String titleScreenFXML = "titleScreen.fxml";

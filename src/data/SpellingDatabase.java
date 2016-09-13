ab83dc3e3df783a35430944d451d7d9e44e6f781
Yuliang Zhou <yuliangzhou7@gmail.com>
2016-09-14 00:26:06 +1200

    Add new words to database complete

--- src/data/SpellingDatabase.java
+++ src/data/SpellingDatabase.java
@@ -17,8 +17,22 @@ public class SpellingDatabase implements Serializable{
         _spellingWords = new HashMap<>();
     }
 
-    public void addNewWord(String levelKey, String line) {
-
-
+    /**
+     * Adds a new word given as a string to the database with a given level. Words
+     * are saved in an arraylist for each level which is contained in a hashmap with
+     * level being the key.
+     * e.g. "Level 1" key contains the word "the"
+     * @param levelKey
+     * @param word
+     */
+    public void addNewWord(String levelKey, String word) {
+        if(!_spellingWords.containsKey(levelKey)){
+            ArrayList<Word> wordsInLevel = new ArrayList<>();
+            wordsInLevel.add(new Word(word));
+            _spellingWords.put(levelKey,wordsInLevel);
+        }else{
+            _spellingWords.get(levelKey).add(new Word(word));
+        }
+        
     }
 }

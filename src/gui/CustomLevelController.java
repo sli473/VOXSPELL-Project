19d96498a401a9b0a83c709c302ca3d486e9eae6
Samule Li <sli473@aucklanduni.ac.nz>
2016-10-22 18:53:57 +1300

    Before changing Database

--- src/gui/CustomLevelController.java
+++ src/gui/CustomLevelController.java
@@ -1,7 +1,12 @@
 package gui;
 
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,6 +16,7 @@ import java.util.ResourceBundle;
 public class CustomLevelController implements Initializable, ControlledScreen {
 
     private MasterController _myParentController;
+    private CustomLevelController thisScreen = this;
 
     @Override
     public void setScreenParent(MasterController screenParent) {
@@ -26,4 +32,15 @@ public class CustomLevelController implements Initializable, ControlledScreen {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    public void uploadList(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose new wordlist");
+        File file = fileChooser.showOpenDialog(new Stage());
+
+        if(file != null){
+
+        }
+    }
+
 }
